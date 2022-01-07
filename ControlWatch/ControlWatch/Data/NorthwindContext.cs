c966b1f576f80018f259e7ff89d321b942c83245
André Silva <andre.f.m.silva@hotmail.com>
2022-01-07 23:48:57 +0000

    Init DbContext

---
+++ ControlWatch/ControlWatch/Data/NorthwindContext.cs
@@ -0,0 +1,17 @@
+﻿using ControlWatch.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlWatch.Data
+{
+    class NorthwindContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<MovieCover> MovieCovers { get; set; }
+    }
+}
