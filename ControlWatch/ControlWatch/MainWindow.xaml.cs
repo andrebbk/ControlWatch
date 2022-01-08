de57d2672e6cb5022d17bea23c6cce6fc7546823
André Silva <andre.f.m.silva@hotmail.com>
2022-01-08 15:45:55 +0000

    Side navegation menu

--- ControlWatch/ControlWatch/MainWindow.xaml.cs
+++ ControlWatch/ControlWatch/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControlWatch.Windows.SideMenu;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +22,45 @@ namespace ControlWatch
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool IsDrawableOpen;
+
         public MainWindow()
-        {
+        {            
             InitializeComponent();
+            LoadMainWindow();
+        }
+
+        private void LoadMainWindow()
+        {
+            IsDrawableOpen = true;
+            this.DrawableMenuContainer.Content = new SideMenu_UserControl(this);
+        }
+
+        //Open Drawable Menu
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            IsDrawableOpen = true;
+            ButtonOpen.Visibility = Visibility.Hidden;
+        }
+
+        //Click on background
+        private void GridBackground_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (IsDrawableOpen)
+            {
+                Storyboard sb = this.FindResource("CloseMenu") as Storyboard;
+                sb.Begin();
+
+                IsDrawableOpen = false;
+
+                ButtonOpen.Visibility = Visibility.Visible;
+            }
+        }
+
+        //Binding Actions    
+        private void OnCloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
